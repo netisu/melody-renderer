2d149e6d782be55eebe586fa8c8fc470a2d86b2d
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2025-08-14 10:33:19 -0400

    Update main.go

--- main.go
+++ main.go
@@ -732,7 +732,7 @@ func generateObjects(userConfig UserConfig) []*aeno.Object {
 	for _, obj := range allObjects {
 		if obj.Mesh != nil {
 			fmt.Printf("  generateObjects: Checking obj mesh %p against cached cranium mesh %p for face texture application.\n", obj.Mesh, cachedCraniumMesh)
-        	if obj.Mesh == cachedCraniumMesh.Mesh {
+        	if obj.Mesh == cachedCraniumMesh {
 				fmt.Printf("  generateObjects: FOUND cranium mesh for face! Applying face texture.\n")
 				obj.Texture = AddFace(userConfig.Items.Face.Item)
 				break
