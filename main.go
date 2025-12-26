95dbe978b3cdb24d7c469eda114c06ee6f1b956c
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2025-12-26 03:07:12 -0500

    Update hsFovy value from 30.5 to 32.5

--- main.go
+++ main.go
@@ -481,7 +481,7 @@ func (s *Server) handleUserRender(w http.ResponseWriter, e RenderEvent) {
 	go func() {
 		defer wg.Done()
 		var (
-			hsFovy   = 30.5
+			hsFovy   = 32.5
 			hsEye    = aeno.V(4, 7, 13)
 			hsCenter = aeno.V(-0.5, 6.8, 0)
 			hsUp     = aeno.V(0, 1, 0)
